97e258cdbbed7e34b7b982d1e351af22ada24aa2
Tuhin Samanta <tuhinsamanta@Tuhins-MacBook-Pro.local>
2024-01-07 22:51:40 +0530

    fine payment implement

--- src/main/java/com/example/librarymanagementsystem/controllers/TransactionController.java
+++ src/main/java/com/example/librarymanagementsystem/controllers/TransactionController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +17,12 @@ public class TransactionController {
     @PostMapping("/transaction")
     public String initiateTransaction(@RequestBody @Valid InitiateTransactionRequest transactionRequest) throws Exception {
         return transactionService.initiateTransaction(transactionRequest);
+    }
+
+    @PostMapping("/transaction/payment")
+    public void makePayment(@RequestParam("amount") Integer amount,
+                            @RequestParam("studentId") Integer studentId,
+                            @RequestParam("transactionId") String txnId) throws Exception {
+        transactionService.payFine(txnId, studentId, amount);
     }
 }
