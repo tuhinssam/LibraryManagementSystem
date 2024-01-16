5118cbaa5595729e94c218b261cc97c9532d560b
Tuhin Samanta <tuhinsamanta@Tuhins-MacBook-Pro.local>
2024-01-17 00:34:01 +0530

    redis configuration

---
+++ src/main/java/com/example/librarymanagementsystem/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package com.example.librarymanagementsystem.config;
+
+import com.example.librarymanagementsystem.models.Student;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Value("${redis.host.url}")
+    String host;
+
+    @Value("${redis.host.port}")
+    Integer port;
+
+    @Value("${redis.auth.password}")
+    String password;
+
+    @Bean
+    public LettuceConnectionFactory  getConnection() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        config.setPassword(password);
+
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> getTemplate(){
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(getConnection());
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new JdkSerializationRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public ObjectMapper getMapper() {
+        return new ObjectMapper();
+    }
+}
